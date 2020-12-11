bd3a051fcacd4b0aebf964c95b5c611e8b6b0845
unknown <venkiteshsanand1920@gmail.com>
2020-12-11 23:20:29 +0530

    Initial Commit

---
+++ Frontend/mysuru/android/app/src/main/kotlin/com/example/mysuru/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mysuru
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Frontend/mysuru/lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+                height: 400,
+                decoration: BoxDecoration(),
+                child: Image.asset(
+                  "assets/bg2.jpg",
+                  fit: BoxFit.fitHeight,
+                )),
+            Container(
+              height: 270,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 70),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.menu, color: Colors.blueAccent),
+                        SizedBox(
+                            height: 40, child: Image.asset("assets/propic.png"))
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Ready for your next",
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                            shadows: <Shadow>[
+                              Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 3.0,
+                                color: Color.fromARGB(87, 70, 50, 0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          "Adventure!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 45,
+                            fontWeight: FontWeight.w500,
+                            shadows: <Shadow>[
+                              Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 3.0,
+                                color: Color.fromARGB(87, 70, 50, 0),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 280),
+              child: Container(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "Top",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "For You",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "Near by",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 30, left: 20, bottom: 30),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            imageBottom("Mysore Palace", "1"),
+                            imageBottom("Shri Chamundeshwari Temple", "3")
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "Rewards",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          ),
+                        ),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                      ),
+                    )
+                  ],
+                ),
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding imageBottom(String txt, String num) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          child: Stack(
+            children: [
+              Container(
+                height: 300,
+                width: 220,
+                child: Image.asset(
+                  "assets/bg$num.jpg",
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, left: 20),
+                child: Container(
+                  width: 200,
+                  child: Text(
+                    txt,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
